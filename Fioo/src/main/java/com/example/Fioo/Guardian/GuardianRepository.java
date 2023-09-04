675f158f5642a106981b16b22aba9a5267364c30
Caio Bulha Silva <34185790+caiobulha@users.noreply.github.com>
2023-09-04 18:36:32 -0300

    Delete Fioo directory

--- Fioo/src/main/java/com/example/Fioo/Guardian/GuardianRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Fioo.Guardian;
-
-import com.example.Fioo.Guardian.Model.Guardian;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuardianRepository extends JpaRepository<Guardian, Long> {
-}
--- Fioo/src/main/java/com/example/Fioo/Student/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Fioo.Student;
-
-import com.example.Fioo.Student.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
